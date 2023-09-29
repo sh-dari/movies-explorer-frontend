140b56bc5ef17b34181755c5fa827f85e435bf2f
Дарья Шипилова <sh.dari@list.ru>
2023-09-29 20:27:17 +0300

    fix: show on localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 export const movieApiLink = "https://api.nomoreparties.co";
-export const mainApiLink = "https://api.movies.shdari.nomoredomains.xyz";
+// export const mainApiLink = "https://api.movies.shdari.nomoredomains.xyz";
+export const mainApiLink = "http://localhost:3000";
 
 export const SCREEN_SM = 730;
 export const SCREEN_MD = 1170;
